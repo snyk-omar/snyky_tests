0d6b1222a3b55c1e613478e94f8dc45a6abb0b76
Omar Quimbaya <omar.quimbaya@snyk.io>
2021-10-28 10:07:26 -0500

    Added Session to code to support url prefixes

--- main.py
+++ main.py
@@ -1,38 +1,66 @@
 import json
 import requests
 from dotenv import dotenv_values
+from urllib.parse import urljoin
 
 
-def make_request(snyk_token: str, url: str) -> str:
+class SnykSession(requests.Session):
     """
-    Function to make a request to one Snyk API URI.
+    Class to create a session with Snyk API.
+    """
+
+    def __init__(self, prefix_url=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.prefix_url = prefix_url
+
+    def request(self, method, url, *args, **kwargs):
+        """
+        Override the request method to add the prefix_url.
+        """
+        url = urljoin(self.prefix_url, url)
+        return super().request(method, url, *args, **kwargs)
+
+
+def create_session(snyk_token: str) -> requests.Session:
+    """
+    Function to create a session with Snyk API.
 
     snyk_token: string
-    url: string
+    return requests.Session
+    """
+    # Using the Snyk v3 API endpoints. Can be adjusted to previous versions.
+    session = SnykSession(prefix_url="https://api.snyk.io/v3/")
+    session.headers.update(
+        {
+            "Content-Type": "application/vnd.api+json",
+            "Authorization": f"token {snyk_token}",
+        }
+    )
+    return session
+
+
+def convert_payload_to_json(payload: dict) -> str:
+    """
+    Function to convert the payload to JSON.
+
+    payload: dict
     return string
     """
-    header = {
-        "Content-Type": "application/json; charset=utf-8",
-        "Authorization": f"token {snyk_token}",
-    }
-    params = {"version": "2021-06-04~beta"}
-
-    req = requests.get(url, headers=header, params=params)
-
-    json_payload = json.loads(req.text)
-    # Pretty printing the output to make it easier to read
-    return json.dumps(json_payload, indent=2)
+    return json.dumps(payload, indent=2)
 
 
 def main() -> None:
     CONFIG = dotenv_values()
     SNYK_TOKEN = CONFIG["SNYK_TOKEN"]
     ORG_ID = CONFIG["ORG_ID"]
-    ALL_PROJECTS = f"https://api.snyk.io/v3/orgs/{ORG_ID}/projects"
+    USER_ID = CONFIG["USER_ID"]
 
-    result = make_request(snyk_token=SNYK_TOKEN, url=ALL_PROJECTS)
-    # printing to standard out to see the result
-    print(result)
+    session = create_session(SNYK_TOKEN)
+    test_uri = f"orgs/{ORG_ID}/users/{USER_ID}"
+
+    # Get user details
+    res = session.get(url=test_uri, params={"version": "2021-09-13~beta"})
+    print(res.json())
 
 
 if __name__ == "__main__":
